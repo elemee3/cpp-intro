5ba67372e48bf5a8f1edb34d519779e8e41a9fe6
elemee3 <leila.m.elshamy@gmail.com>
2019-04-01 19:57:05 -0700

    Simple calculator

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operator (+, -, *, or /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    int result;
+    if (op == '+') {
+        result = num1 + num2;
+    } else if (op == '-') {
+        result = num1 - num2;
+    } else if (op == '/') {
+        result = num1 / num2;
+    } else if (op == '*') {
+        result = num1 * num2;
+    } else {
+        cout << "Invalid operator";
+    }
+
+    cout << result;
+
+    return 0;
+}
